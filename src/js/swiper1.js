0174c0ba1381d844fe3ac935349193ad9ceb01bc
Albina <albinabist1993@mail.ru>
2021-09-01 12:07:15 +0300

    2 задание html academy

---
+++ src/js/swiper1.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper-1", {
+
+    slidesPerView: 1,
+    slidesPerColumn: 1,
+  
+  
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+});
